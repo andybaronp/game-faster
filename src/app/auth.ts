226574b38e3f339f6cafe2c6ba9c2f89a79def13
Jhoandris B <andybaronp@gmail.com>
2023-06-21 15:55:22 -0500

    Const URL

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
-)
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL
+
+const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabase = createClient(url as string, key as string)
 
 export const singOut = async () => {
   await supabase.auth.signOut()
